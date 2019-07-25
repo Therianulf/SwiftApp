a01c3bfd846cd98f5e698286fc34f4a87d3e04d5
Benjamin Larson <ben.larson@creative-alternatives.org>
2019-07-25 11:15:41 -0700

    init commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Benjamin Larson on 7/25/19.
+//  Copyright © 2019 Benjamin Larson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# SwiftLearning
+Learning Swift and obj C for IOS
+
+Just got a mac book. means i gotta learn the lang ;p
